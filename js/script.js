900c83aacbaa38a2f1a79c9f5b5242f4b25de2b2
phoebewalquist <pheebijane@yahoo.com>
2023-04-24 16:22:45 -0700

    changes made

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
-// // Define an array of cards with their values (2 of each)
-// let cardValues = ["A", "B", "C", "D", "E", "F", "G", "H", "A", "B", "C", "D", "E", "F", "G", "H"];
+//inittialize game 
+//shuffle cards -iterate through them, generate a random number 
+//between 0 and 7 and assign it to the flex-item order property:
+const playButton = document.querySelector('.play-button');
+playButton.addEventListener('click', startGame);
+
+
+
+// Define an array of cards with their values (2 of each)
+//const cards = dcument.querySelectorAll
+//   ];
 
 // // Define a variable to keep track of flipped cards
 // let flippedCards = [];
+//return if false
+
+//add an event listener to each card element to detect when a card is clicked.
 
 // // Define a function to flip 2 cards to show their value
 // function flipCard(card) {
@@ -27,6 +39,13 @@
 // }, 1000);
 // }
 
+// limit the number of cards that a player can flip at once 
+//to avoid the player flipping too many cards and losing track of which cards have been flipped.
+
 // // Clear the flippedCards array
 // flippedCards = [];
 // }
+
+//Game over condition to add a game over 
+//condition to detect when all the cards have been matched and the game is won.
+
